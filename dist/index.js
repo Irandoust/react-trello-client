29f326de1a4f7cc0ccd71bf330a36ae83268ac06
Ali Irandoust <irandoust.net@gmail.com>
2019-11-06 13:05:21 +0330

    added traspiled files

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+/* develblock:start */
+
 
 var _react = require('react');
 
@@ -19,6 +21,8 @@ var _TrelloButton = require('./components/TrelloButton');
 var _TrelloButton2 = _interopRequireDefault(_TrelloButton);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* develblock:end */
 
 var TrelloClient = function TrelloClient(props) {
 
